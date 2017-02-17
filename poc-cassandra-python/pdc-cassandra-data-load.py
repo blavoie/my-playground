6b1375f70cc58830091dc9b3a474bf1295336d9d
Bruno Lavoie <bl@brunol.com>
2017-02-16 22:00:47 -0500

    Going forward.

--- poc-cassandra-python/pdc-cassandra-data-load.py
+++ poc-cassandra-python/pdc-cassandra-data-load.py
@@ -1,19 +1,14 @@
 # -*- coding: utf-8 -*-
 
 import cx_Oracle
+from oracle.utils import named_tuple
 
 from cassandra.cluster import Cluster
+from cassandra.cqlengine import connection
 from cassandra.policies import RetryPolicy, TokenAwarePolicy, DCAwareRoundRobinPolicy
 
-from cassandra.cqlengine import connection
-from cassandra.query import dict_factory
-
-from cassandra.cqlengine import columns
-from cassandra.cqlengine.models import Model
-
-### Oracle Connection
-from model import SectionCoursEntite
-from model.SectionCoursEntite import SectionCours
+from model.cassandra_types import SectionCours
+from model.cassandra_types import Individu
 
 ora_user = 'brlav35'
 ora_pass = ''
@@ -23,23 +18,6 @@ ora_nb = 10000
 
 ora_conn = cx_Oracle.connect(ora_user, ora_pass, ora_db)
 
-def columns_names(cur):
-    names = []
-    for desc in cur.description:
-        name = desc[0]
-        names.append(name)
-
-    return names
-
-def named_tuple(tuple, cur, to_lower=True):
-    names = columns_names(cur)
-
-    if to_lower:
-        names = map(lambda x: x.lower(), names)
-
-    return dict(zip(names, tuple))
-
-
 ### Cassandra Connection
 
 cas_keyspace = 'brlav35'
@@ -47,93 +25,107 @@ cas_keyspace = 'brlav35'
 ### Cassandra Connection
 
 cluster = Cluster(contact_points=["n01.cl01", "n02.cl01"],
+                  protocol_version=3,
                   default_retry_policy=RetryPolicy(),
                   load_balancing_policy=TokenAwarePolicy(DCAwareRoundRobinPolicy(local_dc='datacenter1')))
 session = cluster.connect(keyspace=cas_keyspace)
 #session.row_factory = dict_factory
 
 #connection.set_session(session)
-connection.setup(hosts=["n01.cl01","n02.cl01"], default_keyspace=cas_keyspace)
-
-### Define UDT, and register
-"""
-class AdressePostale(object):
-    def __init__(self, no_civique, rue, ville, code_postal):
-        self.no_civique = no_civique
-        self.rue = rue
-        self.ville = ville
-        self.code_postal = code_postal
-
-cluster.register_user_type(cas_keyspace, 'adr_postale_udt', AdressePostale)
-"""
+connection.setup(hosts=["n01.cl01", "n02.cl01"], default_keyspace=cas_keyspace, protocol_version=3)
 
 ### Récupérer et importer utilisateurs
-"""
-ora_cur = ora_conn.cursor()
-ora_cur.arraysize = ora_arraysize
-ora_cur.execute(
-    "select di.numero_dossier, di.code_utilisateur, di.nom, di.prenom from icu.dossier_individu di order by dbms_random.value"
-)
 
-nb = 0
-rows = ora_cur.fetchmany()
-while rows:
+def importer_individu():
+    ora_cur = ora_conn.cursor()
+    ora_cur.arraysize = ora_arraysize
+    ora_cur.execute("""
+    SELECT di.numero_dossier, di.nom, di.prenom, di.code_utilisateur, di.adresse_courriel_principale
+    FROM icu.dossier_individu di
+    WHERE    exists
+                 (SELECT *
+                  FROM icu.inscription_cours_mc inco
+                  WHERE inco.numero_dossier_pidm = di.numero_dossier)
+          OR exists
+                 (SELECT *
+                  FROM icu.enseignant_section_mc ense
+                  WHERE ense.numero_dossier_pidm = di.numero_dossier)
+    """
+                    )
 
-    for row in rows:
-        print row
-
-    # next bulk
-    if nb >= ora_nb:
-        break
-
+    nb = 0
     rows = ora_cur.fetchmany()
-"""
+    while rows:
+        for row in rows:
+            row = named_tuple(row, ora_cur)
+            print row
 
+            # Better way to do this?
+            # Récupérer les colonnes communes entre le modèle et ce qui est retourné par la requête.
+            # Permet de faciliter les multiples entités de destination pour une même source.
+            # Déplacer / Mettre en cache la liste des colonnes communes.
 
-### Récupérer et importer sections de cours
+            cols = set(Individu._columns.keys()).intersection(row.keys())
+            row = {k: row[k] for k in cols}
 
-ora_cur = ora_conn.cursor()
-ora_cur.arraysize = ora_arraysize
-ora_cur.execute("""
-select seco.code_session
-      ,seco.numero_reference_section_cours as nrc
-      ,seco.titre_section_cours as titre
-      ,seco.sigle_matiere
-      ,seco.numero_cours
-      ,coalesce(seco.date_debut_section, sess.date_debut_cours) as date_debut
-      ,coalesce(seco.date_fin_section, sess.date_fin_cours) as date_fin
-      , (select cast (collect (inco.numero_dossier_pidm) as icu.t_number10_tab)
-         from icu.inscription_cours_mc inco
-         where     inco.code_session = seco.code_session
-               and inco.numero_reference_section_cours = seco.numero_reference_section_cours)
-           as liste_inscriptions
-      , (select cast (collect (ense.numero_dossier_pidm) as icu.t_number10_tab)
-         from icu.enseignant_section_mc ense
-         where     ense.code_session = seco.code_session
-               and ense.numero_reference_section_cours = seco.numero_reference_section_cours)
-           as liste_enseignants
-from icu.section_cours_mc seco
-         inner join icu.session_mc sess
-         on seco.code_session = sess.code_session
-where seco.code_statut_section_cours = 'A'
-""")
-
-nb = 0
-rows = ora_cur.fetchmany()
-while rows:
-
-    for row in rows:
-        row = named_tuple(row, ora_cur)
-        print row
-        sc = SectionCours.create(code_session=row['code_session'], nrc=row['nrc'], sigle_matiere=row['sigle_matiere'],
-                                 numero_cours=row['numero_cours'], titre=row['titre'], date_debut=row['date_debut'],
-                                 date_fin=row['date_fin'], inscriptions=row['liste_inscriptions'], enseignants=row['liste_enseignants'])
+            Individu.create(**row)
 
         # next bulk
         if nb >= ora_nb:
             break
 
+        rows = ora_cur.fetchmany()
+
+
+### Récupérer et importer sections de cours
+def importer_section_cours():
+    ora_cur = ora_conn.cursor()
+    ora_cur.arraysize = ora_arraysize
+    ora_cur.execute("""
+    SELECT seco.code_session
+          ,seco.numero_reference_section_cours AS nrc
+          ,seco.titre_section_cours AS titre
+          ,seco.sigle_matiere
+          ,seco.numero_cours
+          ,coalesce(seco.date_debut_section, sess.date_debut_cours) AS date_debut
+          ,coalesce(seco.date_fin_section, sess.date_fin_cours) AS date_fin
+          , (SELECT cast (collect (inco.numero_dossier_pidm) AS icu.t_number10_tab)
+             FROM icu.inscription_cours_mc inco
+             WHERE     inco.code_session = seco.code_session
+                   AND inco.numero_reference_section_cours = seco.numero_reference_section_cours)
+               AS inscriptions
+          , (SELECT cast (collect (ense.numero_dossier_pidm) AS icu.t_number10_tab)
+             FROM icu.enseignant_section_mc ense
+             WHERE     ense.code_session = seco.code_session
+                   AND ense.numero_reference_section_cours = seco.numero_reference_section_cours)
+               AS enseignants
+    FROM icu.section_cours_mc seco
+             INNER JOIN icu.session_mc sess
+             ON seco.code_session = sess.codinscriptions_par_section_course_session
+    WHERE seco.code_statut_section_cours = 'A'
+    """)
+
+    nb = 0
     rows = ora_cur.fetchmany()
+    while rows:
+        for row in rows:
+            row = named_tuple(row, ora_cur)
+            print row
+            sc = SectionCours.create(**row)
+
+            # next bulk
+            if nb >= ora_nb:
+                break
+
+        rows = ora_cur.fetchmany()
+
+
+########################
+## Exécuter
+
+# importer_section_cours()
+importer_individu()
+
 
 ########################
 ## cleanup
